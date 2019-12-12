a665dfb1703a48640d0eb21db3aca78a1928b2a7
kimuksung <kimuksung2@daum.net>
2019-12-12 20:37:04 +0900

    암호화 pdkdp2-password 모듈을 이용한 해쉬 함수 salt + password를 이용하여 암호화

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app =express();
+//import bkfd2Password from 'pbkdf2-password';
+
+//const hasher = bkfd2Password();
+//console.log(hasher);
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const session =require('express-session');
+
+
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(express.json());
+app.use(session({
+    secret:'12321312@3132',
+    resave:false,
+    saveUninitialized:true
+}));
 
-app.get('/login',(req,res)=>{
-    res.send([{
-        "id": "kimuksung2",
-        "password": "ww0603"
-    }, {
-        "id": "kimuksung5",
-        "password": "hannah0603"
-    }]);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
 
-app.listen(port,()=>console.log(`Listening on port ${port}`));
-
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
